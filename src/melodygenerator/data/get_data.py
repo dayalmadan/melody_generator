ecea749f75d1ec7eaa8e4c39f4b9df6fe6db2a88
dayalmadan <dayalmadan@gmail.com>
2022-12-02 07:22:07 +0000

    Some more incomplete changes.

--- src/melodygenerator/data/get_data.py
+++ src/melodygenerator/data/get_data.py
@@ -3,10 +3,8 @@ import random
 from typing import Optional
 
 import melodygenerator.config.get as mcg
-
-from melodygenerator.data.constants import MAJOR_SCALE, MINOR_SCALE, MODEL_MAP
+import melodygenerator.data.constants as const
 from melodygenerator.data.enums import DataType, Model
-
 from melodygenerator.models import Generator, RandomGenerator
 
 
@@ -20,17 +18,31 @@ def get_melody_pattern() -> str:
     return melody_patterns[random.randint(0, len(melody_patterns) - 1)]
 
 
+def get_duration_from_id(duration_id: int) -> float:
+    # TODO
+    # if midi_time + note_duration > BEATS_PER_BAR:
+    #     note_duration = BEATS_PER_BAR - midi_time
+    return 0.0
+
+
+def get_note_number_from_id(scale: str, note_id: Optional[int] = None) -> int:
+    # TODO
+    return 0
+
+
 def initialise_model(model_name: str) -> Optional[Generator]:
     model_id = _get_model_from_name(model_name)
     if model_id == Model.RANDOM:
         note_len_prob = mcg.get_config(section="random", option="note_probabilities", data_type=DataType.LIST)
         silence_prob = mcg.get_config(section="random", option="silence_probability", data_type=DataType.FLOAT)
-        return RandomGenerator(note_len_prob=note_len_prob, silence_prob=silence_prob, notes_per_scale=len(MAJOR_SCALE))
+        return RandomGenerator(
+            note_len_prob=note_len_prob, silence_prob=silence_prob, notes_per_scale=len(const.MAJOR_SCALE)
+        )
     return None
 
 
 def _get_model_from_name(model_name: str) -> Model:
     model_name = model_name.lower()
-    if model_name not in MODEL_MAP:
+    if model_name not in const.MODEL_MAP:
         raise ValueError(f"Model name {model_name} is not a valid model!")
-    return MODEL_MAP[model_name]
+    return const.MODEL_MAP[model_name]
--- src/melodygenerator/scripts/generate_melody.py
+++ src/melodygenerator/scripts/generate_melody.py
@@ -2,9 +2,10 @@ import click
 
 from typing import Dict, List
 
+import melodygenerator.data.get_data as data
+
 from melodygenerator.data.constants import BEATS_PER_BAR
-from melodygenerator.data.enums import MidiData, Model
-from melodygenerator.data.get_data import get_melody_pattern, initialise_model
+from melodygenerator.data.enums import MidiData
 
 
 @click.command()
@@ -12,17 +13,25 @@ from melodygenerator.data.get_data import get_melody_pattern, initialise_model
 @click.option("--scale", default="C", help="Pick a harmonic scale (e.g., C, D#, Ab etc)")
 @click.option("--bpm", default=126, help="Tempo of melody")
 def generate_melody(model_name: str, scale: str, bpm: int):
-    model = initialise_model(model_name)
-    melody_pattern = get_melody_pattern()
+    model = data.initialise_model(model_name)
+    melody_pattern = data.get_melody_pattern()
 
     pattern_memo: Dict[str, List[MidiData]] = {}
     for pattern in melody_pattern:
         if pattern in pattern_memo:
             continue
-        time = 0
+        midi_time = 0
         pattern_memo[pattern] = []
-        while time < BEATS_PER_BAR:
-            pass
+        while midi_time < BEATS_PER_BAR:
+            note_id, duration_id = model.next_note()
+            note_duration = data.get_duration_from_id(duration_id)
+            note_number = data.get_note_number_from_id(scale, note_id)
+            if note_number is None:
+                # insert silence here!
+                midi_time += note_duration
+                continue
+            pattern_memo[pattern].append(MidiData(note=note_number, start_time=midi_time, duration=note_duration))
+            midi_time += note_duration
 
 
 if __name__ == "main":
